2b5fdf6b3308edb18438f44718f873168fc92bdd
Stefanie Gay <stefanie.gay@gmail.com>
2025-01-24 13:46:17 -0500

    amended project commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Person sally = new Person("000001","Sally","Williams", "Ms.", 2005);
+            System.out.println(sally);
+
+
+    }
+}
